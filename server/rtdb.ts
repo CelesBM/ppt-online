d7d8920b131066fedf370ab753e100fecd9f5dc2
unknown <celesbmontero@hotmail.com>
2024-06-30 18:19:40 -0300

    elimino visible .env y arreglo postman render2

--- server/rtdb.ts
+++ server/rtdb.ts
@@ -3,10 +3,6 @@ import { getDatabase, ref, onValue } from "firebase/database";
 import * as dotenv from "dotenv";
 
 dotenv.config();
-//console.log("DATABASE_URL from dotenv:", process.env.DATABASE_URL);
-//console.log("APIKEY from dotenv:", process.env.APIKEY);
-//console.log(process.env.SERVICEACCOUNT_KEY);
-//console.log(JSON.parse(process.env.SERVICEACCOUNT_KEY));
 
 const firebaseConfig = {
   apiKey: process.env.APIKEY,
